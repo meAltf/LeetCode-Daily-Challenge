c5234689e865a40198ddcfb2ee2eee671d27a001
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-01-29 23:16:46 +0530

    Create LFU Cache.java

---
+++ January2023/Hard/LFU Cache.java
@@ -0,0 +1,174 @@
+/************************************************************************************************************************************************
+
+----------------------
+QUESTION NO-460
+----------------------
+
+Design and implement a data structure for a Least Frequently Used (LFU) cache.
+
+Implement the LFUCache class:
+
+LFUCache(int capacity) Initializes the object with the capacity of the data structure.
+int get(int key) Gets the value of the key if the key exists in the cache. Otherwise, returns -1.
+void put(int key, int value) Update the value of the key if present, or inserts the key if not already present. 
+When the cache reaches its capacity, it should invalidate and remove the least frequently used key before inserting a new item. 
+For this problem, when there is a tie (i.e., two or more keys with the same frequency), the least recently used key would be invalidated.
+To determine the least frequently used key, a use counter is maintained for each key in the cache. 
+The key with the smallest use counter is the least frequently used key.
+
+When a key is first inserted into the cache, its use counter is set to 1 (due to the put operation). 
+The use counter for a key in the cache is incremented either a get or put operation is called on it.
+
+The functions get and put must each run in O(1) average time complexity.
+
+
+-----------------------------------------------------------------------------------------------------------------------------
+ 
+
+Example 1:
+
+Input
+["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, 3, null, -1, 3, 4]
+
+Explanation
+// cnt(x) = the use counter for key x
+// cache=[] will show the last used order for tiebreakers (leftmost element is  most recent)
+LFUCache lfu = new LFUCache(2);
+lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
+lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
+lfu.get(1);      // return 1
+                 // cache=[1,2], cnt(2)=1, cnt(1)=2
+lfu.put(3, 3);   // 2 is the LFU key because cnt(2)=1 is the smallest, invalidate 2.
+                 // cache=[3,1], cnt(3)=1, cnt(1)=2
+lfu.get(2);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,1], cnt(3)=2, cnt(1)=2
+lfu.put(4, 4);   // Both 1 and 3 have the same cnt, but 1 is LRU, invalidate 1.
+                 // cache=[4,3], cnt(4)=1, cnt(3)=2
+lfu.get(1);      // return -1 (not found)
+lfu.get(3);      // return 3
+                 // cache=[3,4], cnt(4)=1, cnt(3)=3
+lfu.get(4);      // return 4
+                 // cache=[4,3], cnt(4)=2, cnt(3)=3
+ 
+ ------------------------------------------------------------------------------------------------------------------------
+
+
+**************************************************************************************************************************************************/
+
+class Node<K, V> {
+    private K key;
+    private V data;
+    private int f;
+    
+    public Node (K key, V data) {
+        this.key = key;
+        this.data = data;
+        f = 1;
+    }
+    
+    public void setData(V data) {
+        this.data = data;
+    }
+    
+    public void setF(int f) {
+        this.f = f;
+    }
+    
+    public K getKey() {
+        return key;
+    }
+    
+    public V getData() {
+        return data;
+    }
+    
+    public int getF() {
+        return f;
+    }
+}
+
+class LFUCache {
+    private Map<Integer, LinkedList<Node<Integer, Integer>>> fMap;
+    private int capacity;
+    
+    private Map<Integer, Node<Integer, Integer>> kMap;
+    private int minFrequency;
+    private boolean nonFillable;
+    
+    public LFUCache(int capacity) {
+        fMap = new HashMap<>();
+        kMap = new HashMap<>(capacity);
+        
+        nonFillable = capacity == 0;
+        this.capacity = capacity; 
+    }
+    
+    public void putFMap(int f, Node<Integer, Integer> node) {
+        if (!fMap.containsKey(f)) {
+            fMap.put(f, new LinkedList<Node<Integer, Integer>>());
+        }
+        
+        fMap.get(f).add(node);
+    }
+    
+    public void reconcileMinFrequency(Node<Integer, Integer> node, int f) {
+        fMap.get(f).remove(node);
+        if (fMap.get(f).size() == 0 && minFrequency == f) {
+            minFrequency++;
+        }
+    }
+    
+    public int get(int key) {
+        if (kMap.containsKey(key)) {
+            
+            Node<Integer, Integer> node = kMap.get(key);
+            int f = node.getF();
+            
+            reconcileMinFrequency(node, f);
+            node.setF(++f);
+            
+            putFMap(f, node);
+            return node.getData();
+        }
+        
+        else { return -1; }
+    }
+    
+    public void put(int key, int value) {
+        if (nonFillable)
+            return ;
+        
+        if (kMap.containsKey(key)) {
+            
+            get(key);
+            kMap.get(key).setData(value);
+            
+        }
+        
+        else {
+            if (capacity-- == 0) {
+                ++capacity;
+                
+                Node<Integer, Integer> node = fMap.get(minFrequency).removeFirst();
+                kMap.remove(node.getKey());
+            }
+            
+            minFrequency = 1;
+            Node<Integer, Integer> node =  new Node<> (key, value);
+            
+            kMap.put(key, node);
+            putFMap(1, node);
+        }
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
