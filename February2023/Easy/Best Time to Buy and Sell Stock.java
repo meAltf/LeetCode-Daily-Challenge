8c5c1cb743a4a354b5e58237b837f712de595d51
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-25 20:31:17 +0530

    Update Best Time to Buy and Sell Stock.java

--- February2023/Easy/Best Time to Buy and Sell Stock.java
+++ February2023/Easy/Best Time to Buy and Sell Stock.java
@@ -44,3 +44,24 @@ class Solution {
         return maxProfit;
     }
 }
+
+-------------------------
+Approach-2
+-------------------------
+    
+    class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+
+        for(int i=1; i<prices.length; i++) {
+            if(buy < prices[i]) {
+                profit = Math.max(profit, prices[i]-buy);
+            }
+            else{
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+}
