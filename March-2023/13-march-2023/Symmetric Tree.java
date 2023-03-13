eae01881c5795418e5bc6681aa877a655349df19
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-03-13 23:06:11 +0530

    Create Symmetric Tree.java

---
+++ March-2023/13-march-2023/Symmetric Tree.java
@@ -0,0 +1,58 @@
+/******************************************************************************************************************************************************
+------------------
+Level- Easy
+Question No-101
+---------------------
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+---------------------------------------------------------------------------------------
+
+Example 1:
+
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+----------------------------------------------------------------------------------------
+
+*******************************************************************************************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private boolean check(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null){
+            return true;
+        }
+        if (node1 == null || node2 == null || node1.val != node2.val){
+            return false;
+        }
+        return check(node1.left, node2.right) && check(node1.right, node2.left);
+    }
+    
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+}
