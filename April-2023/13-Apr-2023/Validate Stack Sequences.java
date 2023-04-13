e9020e8b9c14d6a52078792a64e4c23c27e7b193
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-04-13 20:28:03 +0530

    Create Validate Stack Sequences.java

---
+++ April-2023/13-Apr-2023/Validate Stack Sequences.java
@@ -0,0 +1,46 @@
+/******************************************************************************************************************************************************
+------------------------------------
+Level- Medium
+Question No- 946
+------------------------------------
+Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a sequence of push and pop 
+operations on an initially empty stack, or false otherwise.
+
+___________________________________________________________________________________________________ 
+
+Example 1:
+
+Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+Output: true
+Explanation: We might do the following sequence:
+push(1), push(2), push(3), push(4),
+pop() -> 4,
+push(5),
+pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+Example 2:
+
+Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+Output: false
+Explanation: 1 cannot be popped before 2.
+___________________________________________________________________________________________________
+
+*******************************************************************************************************************************************************/
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        Stack<Integer> st = new Stack<>(); 
+        
+        int j = 0; 
+        
+        for(int val : pushed){
+            st.push(val); 
+            while(!st.isEmpty() && st.peek() == popped[j]){ 
+
+                st.pop(); 
+                j++; 
+            }
+        }
+        return st.isEmpty(); 
+    }
+}
