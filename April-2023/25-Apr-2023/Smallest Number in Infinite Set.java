5c300c0193a2c73bdfad1030b1ae844537a3766e
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-04-25 21:37:45 +0530

    Create Smallest Number in Infinite Set.java

---
+++ April-2023/25-Apr-2023/Smallest Number in Infinite Set.java
@@ -0,0 +1,66 @@
+/******************************************************************************************************************************************************
+----------------------
+Level- Medium
+Question No- 2336
+----------------------
+You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
+
+Implement the SmallestInfiniteSet class:
+
+SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain all positive integers.
+int popSmallest() Removes and returns the smallest integer contained in the infinite set.
+void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
+ 
+___________________________________________________________________________________________________
+
+Example 1:
+
+Input
+["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+[[], [2], [], [], [], [1], [], [], []]
+Output
+[null, null, 1, 2, 3, null, 1, 4, 5]
+
+Explanation
+SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+                                   // is the smallest number, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+
+___________________________________________________________________________________________________
+
+*******************************************************************************************************************************************************/
+
+class SmallestInfiniteSet {
+    private boolean isNotPresent[];
+    private int smallestELeInd;
+    public SmallestInfiniteSet() {
+        isNotPresent=new boolean[1001];
+        smallestELeInd=0;
+    }
+    
+    public int popSmallest() {
+        int smallest=smallestELeInd+1;
+        isNotPresent[smallestELeInd++]=true;
+        while(isNotPresent[smallestELeInd])   smallestELeInd++;  
+        return smallest;
+    }
+    
+    public void addBack(int num) {
+        isNotPresent[num-1]=false;
+        smallestELeInd=Math.min(smallestELeInd,num-1);
+    }
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
