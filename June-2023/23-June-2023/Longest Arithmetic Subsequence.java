ac1c41a47e9eaae9f1fa43cc897055e38e02edc1
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-06-23 21:10:47 +0530

    Create Longest Arithmetic Subsequence.java

---
+++ June-2023/23-June-2023/Longest Arithmetic Subsequence.java
@@ -0,0 +1,55 @@
+/******************************************************************************************************************************************************
+---------------------
+Level- MEdium
+Question NO- 1027
+---------------------
+
+Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
+
+Note that:
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+A sequence seq is arithmetic if seq[i + 1] - seq[i] are all the same value (for 0 <= i < seq.length - 1).
+ 
+___________________________________________________________________________________________________
+
+Example 1:
+
+Input: nums = [3,6,9,12]
+Output: 4
+Explanation:  The whole array is an arithmetic sequence with steps of length = 3.
+Example 2:
+
+Input: nums = [9,4,7,2,10]
+Output: 3
+Explanation:  The longest arithmetic subsequence is [4,7,10].
+Example 3:
+
+Input: nums = [20,1,15,3,10,5,8]
+Output: 4
+Explanation:  The longest arithmetic subsequence is [20,15,10,5].
+
+___________________________________________________________________________________________________
+
+*******************************************************************************************************************************************************/
+
+class Solution {
+    public int longestArithSeqLength(int[] A) {
+        int ansMax = 0, n = A.length, was, diff;
+        HashMap<Integer, Integer> tillI[] = new HashMap[n];
+        // initialising HashMap for all indices
+        for(int i = 0; i < n; ++i)
+            tillI[i] = new HashMap<>();
+        
+        // iterating over all non- zero length sequence forming indices
+        for(int i = 1; i < n; ++i)
+            // iterating over all previous values to get all possible solutions
+            for(int j = 0; j < i; ++j) {
+                diff = A[i] - A[j];
+                was = tillI[j].getOrDefault(diff, 1);   // what was longest till j with difference diff
+                ansMax = Math.max(was + 1, ansMax);
+                tillI[i].put(diff, was + 1);    // length found till i with difference diff
+            }
+        return ansMax;
+    }
+}
