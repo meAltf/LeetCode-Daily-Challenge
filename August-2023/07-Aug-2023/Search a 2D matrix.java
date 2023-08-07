295bf49589d3231998a7e8d39cb63285d9e74ade
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-08-07 22:47:38 +0530

    Update Search a 2D matrix.java

--- August-2023/07-Aug-2023/Search a 2D matrix.java
+++ August-2023/07-Aug-2023/Search a 2D matrix.java
@@ -31,74 +31,23 @@ _________________________________
 
 
 
-//User function Template for Java
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
 
-class Solution
-{
-    //function for checking safe condition
-    public static boolean isSafe(int grid[][], int num, int row, int col){
-        for(int i=0; i<9; i++){
-            if(grid[i][col] == num){
-                return false;
-            }
-            if(grid[row][i] == num){
-                return false;
-            }
-        }
-        
-        int StartR = (row/3)*3, StartC = (col/3)*3;
-        for(int i=StartR; i<=StartR+2; i++){
-            for(int j=StartC; j<StartC+3; j++){
-                if(grid[i][j] == num){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    
-    //Function to find a solved Sudoku. 
-    static boolean SolveSudoku(int grid[][])
-    {
-        // add your code here
-        int row = 0, col = 0;
-        return helper(grid, row, col);
-    }
-    
-    static boolean helper(int grid[][], int row, int col){
-        if(row == 9){
-            return true;       
-        }
-        int NextR = row, NextC = col+1;
-        if(col+1 == 9){
-            NextR = row+1; NextC = 0;
-        }
-        
-        if(grid[row][col] != 0){
-            return helper(grid, NextR, NextC);
-        }
-        
-        for(int i=1; i<=9; i++){
-            if(isSafe(grid,i,row,col)){
-                grid[row][col] = i;
-                if(helper(grid,NextR,NextC)){
-                    return true;
-                }
-                grid[row][col] = 0;
-            }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
+
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
         return false;
-    }
-    
-    
-    //Function to print grids of the Sudoku.
-    static void printGrid (int grid[][])
-    {
-        // add your code here
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                System.out.print(grid[i][j] + " ");
-            }
-        }
     }
 }
