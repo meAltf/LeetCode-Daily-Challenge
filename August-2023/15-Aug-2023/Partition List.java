44f4e225decb1adb5bf37b23dbad0ee8c47965b8
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-08-15 22:44:13 +0530

    Create Partition List.java

---
+++ August-2023/15-Aug-2023/Partition List.java
@@ -0,0 +1,50 @@
+/******************************************************************************************************************************************************
+-----------------------
+Level- Medium
+Question No- 86
+------------------------
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+___________________________________________________________________________________________________
+ 
+
+Example 1:
+
+
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+Example 2:
+
+Input: head = [2,1], x = 2
+Output: [1,2]
+
+___________________________________________________________________________________________________
+
+*******************************************************************************************************************************************************/
+
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode before = new ListNode(0);
+        ListNode after = new ListNode(0);
+        ListNode before_curr = before;
+        ListNode after_curr = after;
+        
+        while(head != null) {
+            if(head.val < x) {
+                before_curr.next = head;
+                before_curr = before_curr.next;
+            } else {
+                after_curr.next = head;
+                after_curr = after_curr.next;
+            }
+            head = head.next;
+        }
+        
+        after_curr.next = null;
+        before_curr.next = after.next;
+        
+        return before.next;
+    }
+}
