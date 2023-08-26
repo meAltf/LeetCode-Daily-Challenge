f7acdcaed597eec474c2235088c151f0260f2b72
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-08-26 20:13:09 +0530

    Create  Maximum Length of Pair Chain.java

---
+++ August-2023/26-Aug-2023/Maximum Length of Pair Chain.java
@@ -0,0 +1,57 @@
+/******************************************************************************************************************************************************
+--------------------
+Level- MEdium
+Question No- 646
+------------------
+
+You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
+
+A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
+
+Return the length longest chain which can be formed.
+
+You do not need to use up all the given intervals. You can select pairs in any order.
+___________________________________________________________________________________________________
+
+Example 1:
+
+Input: pairs = [[1,2],[2,3],[3,4]]
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4].
+Example 2:
+
+Input: pairs = [[1,2],[7,8],[4,5]]
+Output: 3
+Explanation: The longest chain is [1,2] -> [4,5] -> [7,8].
+ 
+
+___________________________________________________________________________________________________
+
+*******************************************************************************************************************************************************/
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, Comparator.comparingInt(a -> a[1]));
+        int n = pairs.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[i][0] > pairs[j][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxChainLength = 0;
+        for (int length : dp) {
+            maxChainLength = Math.max(maxChainLength, length);
+        }
+
+        return maxChainLength;
+    }
+}
