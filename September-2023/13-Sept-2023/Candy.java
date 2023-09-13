38358bfe94b1dffeff7b5d38d35b7d0a9a4b0787
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-09-13 23:58:27 +0530

    Create Candy.java

---
+++ September-2023/13-Sept-2023/Candy.java
@@ -0,0 +1,57 @@
+/******************************************************************************************************************************************************
+----------------------
+Level- Hard
+Question No- 135
+--------------------
+___________________________________________________________________________________________________ 
+
+Example 1:
+
+Input: ratings = [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+
+Example 2:
+
+Input: ratings = [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+The third child gets 1 candy because it satisfies the above two conditions.
+ 
+
+___________________________________________________________________________________________________
+
+*******************************************************************************************************************************************************/
+
+class Solution {
+    public int candy(int[] ratings) {
+        int sum=0;
+        int[] a=new int[ratings.length];
+        for(int i=0;i<a.length;i++)
+        {
+            a[i]=1;
+        }
+        for(int i=0;i<ratings.length-1;i++)
+        {
+            if(ratings[i+1]>ratings[i])
+            {
+                a[i+1]=a[i]+1;
+            }
+        }
+        for(int i=ratings.length-1;i>0;i--)
+        {
+            if(ratings[i-1]>ratings[i])
+            {
+                if(a[i-1]<(a[i]+1))
+                {
+                    a[i-1]=a[i]+1;
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            sum+=a[i];
+        }
+        return sum;
+    }
+}
